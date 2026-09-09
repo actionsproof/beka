74a7614ac7578928d8548de8df385a533bdbd46c
masidytest <masidytest@users.noreply.github.com>
2026-09-09 21:53:33 +0200

    Fix: Add suppressHydrationWarning to ThemeProvider to prevent build errors

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -7,15 +7,9 @@ export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  const [mounted, setMounted] = React.useState(false)
-
-  React.useEffect(() => {
-    setMounted(true)
-  }, [])
-
-  if (!mounted) {
-    return <>{children}</>
-  }
-
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider {...props} suppressHydrationWarning>
+      {children}
+    </NextThemesProvider>
+  )
 }
